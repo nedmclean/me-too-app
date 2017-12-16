21b9d8d2dadb57d5d2655aba4a988f70bdfe12aa
Veronica Williams <vcwilliams2014@gmail.com>
2017-12-16 05:03:07 -0800

    Updating Css and Frontend.js

--- public/assets/js/frontend.js
+++ public/assets/js/frontend.js
@@ -1,36 +1,46 @@
+// table One (below) is used for the manager to post a forum. Data is displayed below.
+
 var dummyData = [
 {
 user_id: 100,
+username:"string",
 title: "Moving Forward...Past the Pain",
-post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate unde animi nam, quidem maiores mollitia veniam doloribus optio rem deleniti? Corporis aspernatur ex soluta similique, iusto laudantium illum assumenda nihil."
+body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate unde animi nam, quidem maiores mollitia veniam doloribus optio rem deleniti? Corporis aspernatur ex soluta similique, iusto laudantium illum assumenda nihil.",
+description:"people are in need of understanding. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem at quisquam ut, ex inventore ipsa. "
 },
+
 
 {
 user_id: 101,
+username:"string",
 title: "A Bright Future is Ahead",
-post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate unde animi nam, quidem maiores mollitia veniam doloribus optio rem deleniti? Corporis aspernatur ex soluta similique, iusto laudantium illum assumenda nihil."
-
+body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate unde animi nam, quidem maiores mollitia veniam doloribus optio rem deleniti? Corporis aspernatur ex soluta similique, iusto laudantium illum assumenda nihil.",
+description:"Lorem ipsum dolor sit amet"
 },
 
 {
-
 user_id: 102,
+username:"string",
 title: "One day at a Time",
-post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate unde animi nam, quidem maiores mollitia veniam doloribus optio rem deleniti? Corporis aspernatur ex soluta similique, iusto laudantium illum assumenda nihil."
-
+body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate unde animi nam, quidem maiores mollitia veniam doloribus optio rem deleniti? Corporis aspernatur ex soluta similique, iusto laudantium illum assumenda nihil.",
+description:"Be your best...do your best."
 }
 
 ];
+
+// table two will be used to 
+
 
 // This code renders the titles to the start.html page
 for(var i = 0; i < dummyData.length; i++){
 
 var title = dummyData[i].title
 var user_id = dummyData[i].user_id
+var username=dummyData[i].username
+var descrip = dummyData[i].description
 
 // create the links using the a href tags
-$('#posts').append("<div class='postTitles' id="+user_id+"><"+title+"></div>");	
-
+$('#posts').append("<div class='postTitles' id="+user_id+">"+title+"<p class='description'>"+descrip+"</p></div>");	
 }
 
 // this is for the post page.
@@ -53,6 +63,7 @@ $(document).on('click', '.postTitles', function(){
 	var $this = $(this).attr("id")
 	console.log($this);
 })
+
 
 
 
