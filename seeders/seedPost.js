fee610c526d25fd0f2da8b1db4cff12e77a26ef4
April Denise Belcher <aprilbelcher@me.com>
2017-12-17 16:16:42 -0800

    added seeders for forum index post and users

---
+++ seeders/seedPost.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up(queryInterface, Sequelize) {
+      // Add altering commands here.
+      // Return a promise to correctly handle asynchronicity.
+    return queryInterface.bulkInsert('Posts', [{
+      username: "Sabrina",
+      body: "lorem ipsumlorem ipsumlorem ipsumlorem ipsum",
+      ForumId: 1,
+      location: "nwiofnosfn",
+      createdAt: Sequelize.fn(('NOW')),
+      updatedAt: Sequelize.fn(('NOW'))
+    }, {
+      username: "Catherine",
+      body: "lorem ipsumlorem ipsumlorem ipsumlorem ipsum",
+      ForumId: 1,
+      location: "nwiofnosfn",
+      createdAt: Sequelize.fn(('NOW')),
+      updatedAt: Sequelize.fn(('NOW'))
+    }], {});
+  },
+
+  down(queryInterface, Sequelize) { // eslint-disable-line no-unused-vars
+      // Add reverting commands here.
+      // Return a promise to correctly handle asynchronicity.
+    return queryInterface.bulkDelete('Posts', null, {});
+  }
+};
