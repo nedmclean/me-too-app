b23d3eec29b32cfeac332924c15cba227449ae3d
Veronica Williams <vcwilliams2014@gmail.com>
2017-12-18 09:25:46 -0800

    Created the Post form linked from the post_form.html

---
+++ public/assets/js/frontend-routes.js
@@ -0,0 +1,12 @@
+// code for the button for the introduction page
+$("#about").on("click", function(){
+
+window.location.href = "about.html"
+
+});
+
+$("#enter").on("click", function(){
+
+window.location.href = "start.html"
+
+});
